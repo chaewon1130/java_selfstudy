26f6e61bc6392a5b2142783dbaf5594a03c3276d
DH <chaewon1130@naver.com>
2022-02-09 20:03:47 +0900

    test

---
+++ Variable/Operator.java
@@ -0,0 +1,32 @@
+package Variable;
+
+public class Operator {
+	public static void main(String[] args) {
+//		int num1 = 10, num2 = 20, num3 = 30;
+//		num1 = num2 = num3;
+//		System.out.println(num1);
+//		System.out.println(num2);
+//		System.out.println(num3);
+		
+//		int a = ((25*5)+(36-4)-72)/5;
+//		System.out.println(a);
+		
+//		int sum=3;
+//		for(int i=2; i<5; i++) {
+//			sum += i*3;
+//			System.out.println(sum);
+//		}
+		
+//		double n1 = ((25+5)+(36/4)-72)*5;
+//		double n2 = ((25*5)+(36-4)+71)/4;
+//		double n3 = (128/4)*2;
+//		
+//		if(n1 > n2 && n2 > n3) {
+//			System.out.println(true);
+//		}else {
+//			System.out.println(false);
+//		}
+		
+		System.out.println(~7);
+	}
+}
---
+++ Variable/Variable01.java
@@ -0,0 +1,7 @@
+package Variable;
+
+public class Variable01 {
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
