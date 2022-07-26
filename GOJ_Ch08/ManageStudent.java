35de2f2905c5bc28caf551ed227a2e547dd67a1e
DH <chaewon1130@naver.com>
2022-07-26 13:57:12 +0900

    Object

--- GOJ_Ch08/ManageStudent.java
+++ GOJ_Ch08/ManageStudent.java
@@ -2,10 +2,11 @@ package GOJ_Ch08;
 
 public class ManageStudent {
 	public static void main(String[] args) {
-		Student[] student;
+//		Student[] student;
 		ManageStudent ms = new ManageStudent();
-		student = ms.addStudent();
-		ms.printStudents(student);
+//		student = ms.addStudent();
+//		ms.printStudents(student);
+		ms.checkEquals();
 	}
 	public Student[] addStudent() {
 		Student[] student = new Student[3];
@@ -17,6 +18,17 @@ public class ManageStudent {
 	public void printStudents(Student student[]) {
 		for(int i=0; i<3; i++) {
 			System.out.println(student[i].toString());
+		}
+	}
+	
+	public void checkEquals() {
+		Student a = new Student("Min", "Seoul", "010XXXXXXXX", "ask@godofjava.com");
+		Student b = new Student("Min", "Seoul", "010XXXXXXXX", "ask@godofjava.com");
+		
+		if(a.equals(b)) {
+			System.out.println("Equal");
+		}else {
+			System.out.println("Not Equal");
 		}
 	}
 }
--- GOJ_Ch08/Student.java
+++ GOJ_Ch08/Student.java
@@ -17,4 +17,29 @@ public class Student {
 	public String toString() {
 		return name + " " + address + " " + phone + " " + email;
 	}
+	
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		if(obj == null) return false;
+		if(getClass() != obj.getClass()) return false;
+		Student other = (Student)obj;
+		
+		if(name == null) {
+			if(other.name != null) return false;
+		}else if(!name.equals(other.name)) return false;
+		
+		if(address == null) {
+			if(other.address != null) return false;
+		}else if(!address.equals(other.address)) return false;
+		
+		if(phone == null) {
+			if(other.phone != null) return false;
+		}else if(!phone.equals(other.phone)) return false;
+		
+		if(email == null) {
+			if(other.email != null) return false;
+		}else if(!email.equals(other.email)) return false;
+		
+		return true;
+	}
 }
